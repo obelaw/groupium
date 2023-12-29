b8b856d1ee82b2a16880c3e8d69d85818e290265
Karim Mohamed <komicho1996@gmail.com>
2023-12-29 14:57:35 +0200

    first commit

---
+++ src/GroupiumServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Obelaw\Groupium;
+
+use Illuminate\Support\ServiceProvider;
+
+class GroupiumServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
